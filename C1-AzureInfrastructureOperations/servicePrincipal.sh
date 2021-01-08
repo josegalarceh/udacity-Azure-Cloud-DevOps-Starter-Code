f546ba1510202bf08c545d466dec823e9d192e18
jose manuel galarce <jmgalarceh@achs.cl>
2021-01-07 21:45:43 -0300

    update readme with udacity review, update the serviceprincipal.sh file

--- C1-AzureInfrastructureOperations/servicePrincipal.sh
+++ C1-AzureInfrastructureOperations/servicePrincipal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export ARM_SKIP_PROVIDER_REGISTRATION=true
-export ARM_CLIENT_ID=
-export ARM_CLIENT_SECRET=
-export ARM_SUBSCRIPTION_ID=
-export ARM_TENANT_ID=
+export ARM_CLIENT_ID=<remplace with the Client ID>
+export ARM_CLIENT_SECRET=<remplace with the Client secret>
+export ARM_SUBSCRIPTION_ID=<remplace with the subscription ID>
+export ARM_TENANT_ID=<remplace with the Tenant ID>
 ls -la
 env | grep ARM
